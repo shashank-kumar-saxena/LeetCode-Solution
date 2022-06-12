327df0dc33eec17af1ed0625a2cdd88299173b59
Shashank <shashanksaxena226@gmail.com>
2022-06-12 12:10:05 +0530

    Merge K Sorted Lists

---
+++ Merge K Sorted Lists/Solution.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode list1=null;
+        for(ListNode listn:lists)
+        {
+            list1=merge(list1,listn);
+        }
+        return(list1);
+    }
+    static ListNode merge(ListNode list1,ListNode list2)
+    {
+        if(list1==null&&list2==null)
+        {
+            return(list1);
+        }
+        ListNode list3=null;
+        ListNode temp=null;
+        while(true)
+        {
+            temp=new ListNode();
+            if(list1==null&&list2==null)
+            {
+               break;
+            }
+            if(list1==null)
+            {
+                temp.val=list2.val;
+                temp.next=list3;
+                list3=temp;
+                list2=list2.next;
+                continue;
+            }
+            if(list2==null)
+            {
+                temp.val=list1.val;
+                temp.next=list3;
+                list3=temp;
+                list1=list1.next;
+                continue;
+            }
+            if(list1.val>=list2.val)
+            {
+                temp.val=list2.val;
+                temp.next=list3;
+                list3=temp;
+                list2=list2.next;
+                continue;
+            }
+            if(list1.val<list2.val)
+            {
+                temp.val=list1.val;
+                temp.next=list3;
+                list3=temp;
+                list1=list1.next;
+                continue;
+            }
+        }
+        ListNode current=list3,next,prev=null;
+        while(current!=null)
+        {
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return(prev);
+ 
+    }
+}
