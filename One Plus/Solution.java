7650ccfb8f2af44123e1dedbfce40adb8f107f27
Shashank <shashanksaxena226@gmail.com>
2022-06-16 16:49:17 +0530

    One Plus

---
+++ One Plus/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    for(var i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+            digits[i]=0;
+        }
+    digits.unshift(1);
+    return(digits);
+};
