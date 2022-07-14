5831191d1d9293ca794b15ed3886c40fa62fecfb
Shashank <shashanksaxena226@gmail.com>
2022-07-14 07:46:29 +0530

    Fizz Buzz

---
+++ Fizz Buzz/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> list=new ArrayList();
+        for(int i=0;i<n;)
+        {
+            i++;
+            if(i%3==0&&i%5==0)
+                list.add("FizzBuzz");
+            else if(i%3==0)
+                list.add("Fizz");
+            else if(i%5==0)
+                list.add("Buzz");
+            else 
+                list.add(Integer.toString(i));
+        }
+        return(list);
+    }
+}
